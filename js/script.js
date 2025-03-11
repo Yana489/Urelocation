a9f22e56c797e874b2f4dfa9e534b2e55feae8ba
yanastarodoubets <yana.akulenko@gmail.com>
2025-03-11 20:06:54 +0100

    *

--- js/script.js
+++ js/script.js
@@ -1,20 +1,67 @@
+document
+  .querySelector(".header__burger")
+  .addEventListener("click", function () {
+    this.classList.toggle("active");
+    document.querySelector(".menu").classList.toggle("open");
+  });
 
-document.querySelector('.header__burger').addEventListener('click', function() {
-   this.classList.toggle('active');
-   document.querySelector('.menu').classList.toggle('open');
+document.addEventListener("DOMContentLoaded", function () {
+  const dropdownLink = document.getElementById("dropdownLink");
+  const dropdownList = document.getElementById("dropdownList");
+
+  dropdownLink.addEventListener("click", function () {
+    dropdownList.style.opacity = dropdownList.style.opacity === "1" ? "0" : "1";
+    dropdownList.style.visibility =
+      dropdownList.style.visibility === "visible" ? "hidden" : "visible";
+  });
 });
 
-const initSlider = () => {
-   const imageList = document.querySelector(".start__item");
-   const slideButtons = document.querySelectorAll(".start__icon");
 
-   slideButtons.forEach(button => {
-      button.addEventListener("click", () => {
-         const direction = button.id === "prev-slide" ? -1 : 1;
-         const scrollAmount = imageList.clientWidth * direction;
-         imageList.scrollBy({ left:scrollAmount, behavior: "smooth" });
+  document.addEventListener('DOMContentLoaded', function() {
+    const prevButton = document.getElementById('prev-slide');
+    const nextButton = document.getElementById('next-slide');
+    const sliderWrapper = document.querySelector('.start__slider-wrapper');
+    const scrollAmount = 300;
+    prevButton.addEventListener('click', function() {
+      sliderWrapper.scrollBy({
+        left: -scrollAmount,
+        behavior: 'smooth'
       });
-   });
-}
+    });
 
-window.addEventListener("load", initSlider);
+    nextButton.addEventListener('click', function() {
+      sliderWrapper.scrollBy({
+        left: scrollAmount,
+        behavior: 'smooth'
+      });
+    });
+  });
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const faqItems = document.querySelectorAll(".faq__item");
+
+  faqItems.forEach((item) => {
+    const questionButton = item.querySelector(".faq__question");
+    const answer = item.querySelector(".faq__answer");
+    const arrow = item.querySelector(".faq__arrow");
+    const faqBlock = item.querySelector(".faq__block");
+
+    questionButton.addEventListener("click", () => {
+      answer.classList.toggle("show");
+      arrow.classList.toggle("rotated");
+      faqBlock.classList.toggle("active");
+    });
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const footerArrow = document.querySelector(".footer__arrow");
+
+  footerArrow.addEventListener("click", function () {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  });
+});
